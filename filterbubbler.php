2cf5fbbeeb2fa052439b76878911a04192297a3d
Ean Schuessler <ean@brainfood.com>
2017-04-16 17:37:53 -0500

    Use singular names for objects and proper rest returns

--- filterbubbler.php
+++ filterbubbler.php
@@ -57,42 +57,48 @@ function filterbubbler_cpt() {
  * @param array $data Options for the function.
  * @return string|null corpora names,  * or null if none.
  */
-function fb_search_corpura( $data ) {
-    $corpura = get_posts(array(
+function fb_list_corpura( $data ) {
+    $corpus_posts = get_posts(array(
         'post_type' => 'fb_corpus',
         'orderby' => 'title'
     ));
-    return $corpura;
+
+    $corpura = array();
+
+    foreach ($corpus_posts as $post) {
+        array_push($corpura, array('name' => $post->post_name, 'description' => $post->post_content));
+    }
+
+    return new WP_REST_Response($corpura, 200);
 }
 
 // Register the fb_get_corpura REST function
 add_action( 'rest_api_init', function () {
   register_rest_route( 'filterbubbler/v1', '/corpus', array(
     'methods' => 'GET',
-    'callback' => 'fb_search_corpura',
+    'callback' => 'fb_list_corpura',
   ) );
 } );
 
-
 /**
- * Get a list of corpura
+ * Read a corpus
  *
  * @param array $data Options for the function.
  * @return string|null corpora names,  * or null if none.
  */
-function fb_get_corpura( $data ) {
-    $corpura = get_posts(array(
+function fb_read_corpus( $data ) {
+    $corpus = get_posts(array(
         'post_type' => 'fb_corpus',
         'orderby' => 'title'
     ));
-    return $corpura;
+    return new WP_REST_Response($corpus, 200);
 }
 
 // Register the fb_get_corpura REST function
 add_action( 'rest_api_init', function () {
   register_rest_route( 'filterbubbler/v1', '/corpus/(?P<id>\d+)', array(
     'methods' => 'GET',
-    'callback' => 'fb_get_corpura',
+    'callback' => 'fb_read_corpura',
   ) );
 } );
 
@@ -102,14 +108,12 @@ add_action( 'rest_api_init', function () {
  * @param array $data Options for the function.
  * @return string|null corpora names,  * or null if none.
  */
-function fb_create_corpura( $data ) {
+function fb_create_corpus( $data ) {
     $corpus_name = strtolower(wp_strip_all_tags($data['title']));
     $corpus_description = wp_strip_all_tags($data['description']);
 
     if (fb_corpus_exists($corpus_name)) {
-        return array(
-            'status' => 'Corpus '.$corpus_name.' already exists'
-        );
+        return new WP_Error( 'code', 'Corpus '.$corpus_name.' already exists');
     }
 
     // Create post object
@@ -124,17 +128,16 @@ function fb_create_corpura( $data ) {
     // Insert the post into the database
     $post_id = wp_insert_post( $post );
 
-    return array(
+    return new WP_REST_Response(array(
         'id' => $post_id,
-        'status' => 'success'
-    );
+    ), 200);
 }
 
 // Register the fb_create_corpura REST function
 add_action( 'rest_api_init', function () {
   register_rest_route( 'filterbubbler/v1', '/corpus', array(
     'methods' => 'POST',
-    'callback' => 'fb_create_corpura',
+    'callback' => 'fb_create_corpus',
   ) );
 } );
 
@@ -144,19 +147,39 @@ add_action( 'rest_api_init', function () {
  * @param array $data Options for the function.
  * @return string|null corpora names,  * or null if none.
  */
-function fb_get_classifications( $data ) {
-    $corpura = get_posts(array(
+function fb_list_classifications( $data ) {
+    $corpura_posts = get_posts(array(
         'post_type' => 'fb_classification',
         'orderby' => 'title'
     ));
-    return $corpura;
+
+    $corpura = array();
+
+    foreach($corpura_posts as $post) {
+        array_push($corpura, array(
+            'corpus' => get_post_meta($post->ID, 'corpus', true),
+            'url' => get_post_meta($post->ID, 'url', true),
+            'classification' => get_post_meta($post->ID, 'classification', true),
+        ));
+    }
+
+    return new WP_REST_Response(array(
+        'corpus' => $data['corpus'],
+        'classifications' => $corpura,
+    ), 200);
 }
 
 // Register the fb_get_classifications REST function
 add_action( 'rest_api_init', function () {
   register_rest_route( 'filterbubbler/v1', '/classification', array(
     'methods' => 'GET',
-    'callback' => 'fb_get_classifications',
+    'callback' => 'fb_list_classifications',
+  ) );
+
+  // With corpus
+  register_rest_route( 'filterbubbler/v1', '/classification/(?P<corpus>\w+)', array(
+    'methods' => 'GET',
+    'callback' => 'fb_list_classifications',
   ) );
 } );
 
@@ -172,33 +195,32 @@ function fb_create_classification( $data ) {
     $url = wp_strip_all_tags($data['url']);
 
     if (!fb_corpus_exists($corpus)) {
-        return array(
-            'status' => 'Unknown corpus '.$corpus
-        );
+        return new WP_Error( 'code', 'Unknown corpus '.$corpus);
     }
 
     if (fb_classification_exists($corpus, $url, $classification)) {
-        return array(
-            'status' => 'Classification '.$classification.' already exists for '.$corpus
-        );
+        return new WP_Error( 'code', 'Classification '.$classification.' already exists for '.$corpus);
     }
 
     // Create post object
     $post = array(
       'post_type'     => 'fb_classification',
-      'post_title'    => $url.' is '.$classification.' for '.$corpus,
+      'post_title'    => $corpus.','.$url.','.$classification,
       'post_name'     => sha1($corpus.$url.$classification),
-      'post_content'  => '"'.$corpus.'","'.$url.'","'.$classification.'"',
-      'post_status'   => 'publish'
+      'post_status'   => 'publish',
+      'meta_input'    => array(
+          'corpus' => $corpus,
+          'url' => $url,
+          'classification' => $classification
+      )
     );
      
     // Insert the post into the database
     $post_id = wp_insert_post( $post );
 
-    return array(
+    return new WP_REST_Response(array(
         'id' => $post_id,
-        'status' => 'success'
-    );
+    ), 200);
 }
 
 // Register the fb_get_classifications REST function
